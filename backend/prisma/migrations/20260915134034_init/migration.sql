f960f185a82ddbb2ce4d1e83d55b9aaa443c6b1c
Yanuar Audi Nugraha <85717515+yanuaraudi@users.noreply.github.com>
2026-09-15 20:44:38 +0700

    initialize prisma, prisma modelling

---
+++ backend/prisma/migrations/20260915134034_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `price` DECIMAL(65, 30) NOT NULL,
+    `stock` INTEGER NOT NULL,
+    `category` VARCHAR(191) NULL,
+    `imageUrl` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
